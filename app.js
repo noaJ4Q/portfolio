efdc90d138f0bc84f7c5bd3a7c27d6c2bc09aff9
noe <noe.jaraq@gmail.com>
2024-01-25 15:23:02 -0500

    new animation

--- app.js
+++ app.js
@@ -112,6 +112,19 @@ tooltips.forEach((tooltip) => {
 
 });
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
+
 // initialize emailjs
 (function(){
     emailjs.init(EMAILJS_PUBLIC_KEY);
