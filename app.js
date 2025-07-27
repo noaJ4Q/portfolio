e395920f473a8d9c7ba0e52b4fe660d97aa6a3a2
Noe Jara <noe.jaraq@gmail.com>
2025-07-27 02:23:41 -0500

    changes

--- app.js
+++ app.js
@@ -1,84 +1,84 @@
 const data = {
-    english: {
-        userTitle: 'Hi, I\'m Noe Jara',
-        userSubtitle: 'Full-Stack Developer',
-        userDescription: 'A telecommunication engineering student at PUCP with interest ant experience in web development.',
-        sections: [
-            'Projects',
-            'Skills',
-            'Contact'
-        ],
-        projects: [
-            {
-                title: 'Registration and administration of clinic locations',
-                description: 'Clinic system for appointment scheduling and management of patients, doctors and administrative staff',
-            },
-            {
-                title: 'Registration and monitoring of incidents on the PUCP campus',
-                description: 'Reporting and solution system for incidents that occur on the PUCP campus.'
-            },
-            {
-                title: 'Authentication web system with Spring Boot and MySQL',
-                description: 'Web system that allows the registration and authentication of users.'
-            },
-            {
-                title: 'Analize and generate images with Azure AI',
-                description: 'Analyze images using its URL or generate new ones based on text.'
-            },
-            {
-                title: 'To-Do List app',
-                description: 'Organize your work and life through this simple to do list app.'
-            }
-        ],
-        buttons: {
-            code: 'Code',
-            live: 'Live',
-            send: 'Send'
-        },
-        contact: {
-            message: 'Email message'
-        }
+  english: {
+    userTitle: 'Hi, I\'m Noe Jara',
+    userSubtitle: 'Full-Stack Developer',
+    userDescription: 'A telecommunication engineering student at PUCP with interest ant experience in web development.',
+    sections: [
+      'Projects',
+      'Skills',
+      'Contact'
+    ],
+    projects: [
+      {
+        title: 'Registration and administration of clinic locations',
+        description: 'Clinic system for appointment scheduling and management of patients, doctors and administrative staff',
+      },
+      {
+        title: 'Registration and monitoring of incidents on the PUCP campus',
+        description: 'Reporting and solution system for incidents that occur on the PUCP campus.'
+      },
+      {
+        title: 'Authentication web system with Spring Boot and MySQL',
+        description: 'Web system that allows the registration and authentication of users.'
+      },
+      {
+        title: 'Analize and generate images with Azure AI',
+        description: 'Analyze images using its URL or generate new ones based on text.'
+      },
+      {
+        title: 'To-Do List app',
+        description: 'Organize your work and life through this simple to do list app.'
+      }
+    ],
+    buttons: {
+      code: 'Code',
+      live: 'Live',
+      send: 'Send'
     },
-    spanish: {
-        userTitle: 'Hola, soy Noe Jara',
-        userSubtitle: 'Desarrollador Full Stack',
-        userDescription: 'Estudiante de Ingeniería de las Telecomunicaciones en la PUCP con interés y experiencia en el desarrollo web.',
-        sections: [
-            'Proyectos',
-            'Habilidades',
-            'Contacto'
-        ],
-        projects: [
-            {
-                title: 'Registro y administración de las sedes de una clínica',
-                description: 'Sistema clínico para la programación de citas y gestión de pacientes, médicos y personal administrativo.',
-            },
-            {
-                title: 'Registro y monitoreo de incidentes en el campus PUCP',
-                description: 'Sistema de reporte y solución de incidentes ocurridos en el campus PUCP.'
-            },
-            {
-                title: 'Sistema web de autenticación con Spring Boot y MySQL',
-                description: 'Sistema web que permite el registro y autenticación de usuarios.'
-            },
-            {
-                title: 'Analize and generate images with Azure AI',
-                description: 'Analiza imágenes usando su URL o genera nuevas basadas en texto.'
-            },
-            {
-                title: 'Aplicación de lista de tareas',
-                description: 'Organiza tu trabajo y vida a través de esta simple aplicación de lista de tareas.'
-            }
-        ],
-        buttons: {
-            code: 'Código',
-            live: 'Demo',
-            send: 'Enviar'
-        },
-        contact: {
-            message: 'Mensaje'
-        }
+    contact: {
+      message: 'Email message'
     }
+  },
+  spanish: {
+    userTitle: 'Hola, soy Noe Jara',
+    userSubtitle: 'Desarrollador Full Stack',
+    userDescription: 'Estudiante de Ingeniería de las Telecomunicaciones en la PUCP con interés y experiencia en el desarrollo web.',
+    sections: [
+      'Proyectos',
+      'Habilidades',
+      'Contacto'
+    ],
+    projects: [
+      {
+        title: 'Registro y administración de las sedes de una clínica',
+        description: 'Sistema clínico para la programación de citas y gestión de pacientes, médicos y personal administrativo.',
+      },
+      {
+        title: 'Registro y monitoreo de incidentes en el campus PUCP',
+        description: 'Sistema de reporte y solución de incidentes ocurridos en el campus PUCP.'
+      },
+      {
+        title: 'Sistema web de autenticación con Spring Boot y MySQL',
+        description: 'Sistema web que permite el registro y autenticación de usuarios.'
+      },
+      {
+        title: 'Analize and generate images with Azure AI',
+        description: 'Analiza imágenes usando su URL o genera nuevas basadas en texto.'
+      },
+      {
+        title: 'Aplicación de lista de tareas',
+        description: 'Organiza tu trabajo y vida a través de esta simple aplicación de lista de tareas.'
+      }
+    ],
+    buttons: {
+      code: 'Código',
+      live: 'Demo',
+      send: 'Enviar'
+    },
+    contact: {
+      message: 'Mensaje'
+    }
+  }
 }
 
 // switch language constants
@@ -104,130 +104,130 @@ const EMAILJS_TEMPLATE_ID = 'template_w4w12zk';
 // css configuration
 const tooltips = document.querySelectorAll('.tooltip-text');
 tooltips.forEach((tooltip) => {
-    const iconWidth = tooltip.previousElementSibling.offsetWidth;
-    const tooltipWidth = tooltip.offsetWidth;
-    const shift = (iconWidth - tooltipWidth) / 2;
-    tooltip.style.left = shift + 'px';
+  const iconWidth = tooltip.previousElementSibling.offsetWidth;
+  const tooltipWidth = tooltip.offsetWidth;
+  const shift = (iconWidth - tooltipWidth) / 2;
+  tooltip.style.left = shift + 'px';
 
 });
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+    else {
+      entry.target.classList.remove('show');
+    }
+  });
 });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((element) => observer.observe(element));
 
 // initialize emailjs
-(function(){
-    emailjs.init(EMAILJS_PUBLIC_KEY);
+(function () {
+  emailjs.init(EMAILJS_PUBLIC_KEY);
 })();
 
 title.onclick = () => {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 angle.onclick = () => {
-    const windowHeight = window.innerHeight;
-    const navHeight = document.getElementById('nav').offsetHeight;
-    window.scrollTo({top: windowHeight-navHeight, behavior: 'smooth'});
+  const windowHeight = window.innerHeight;
+  const navHeight = document.getElementById('nav').offsetHeight;
+  window.scrollTo({ top: windowHeight - navHeight, behavior: 'smooth' });
 }
 
 window.onscroll = () => {
-    if (window.scrollY > 0) {
-        angle.style.display = 'none';
-    } else {
-        angle.style.display = 'block';
-    }
+  if (window.scrollY > 0) {
+    angle.style.display = 'none';
+  } else {
+    angle.style.display = 'block';
+  }
 }
 
 langBtn.onclick = () => {
-    let lang = langBtn.innerText;
+  let lang = langBtn.innerText;
 
-    if (lang === 'eng'){ // switch to spanish
-        langBtn.innerText = 'esp';
+  if (lang === 'eng') { // switch to spanish
+    langBtn.innerText = 'esp';
 
-        // user data
-        userTitle.innerText = data.spanish.userTitle;
-        userSubtitle.innerText = data.spanish.userSubtitle;
-        userDescription.innerText = data.spanish.userDescription;
+    // user data
+    userTitle.innerText = data.spanish.userTitle;
+    userSubtitle.innerText = data.spanish.userSubtitle;
+    userDescription.innerText = data.spanish.userDescription;
 
-        // sections
-        sections.forEach((section, index) => {
-            section.innerText = data.spanish.sections[index];
-        });
+    // sections
+    sections.forEach((section, index) => {
+      section.innerText = data.spanish.sections[index];
+    });
 
-        // projects
-        projects.forEach((project, index) => {
-            project.querySelector('h3').innerText = data.spanish.projects[index].title // change title
-            project.querySelector('p').innerText = data.spanish.projects[index].description // change description
-        });
+    // projects
+    projects.forEach((project, index) => {
+      project.querySelector('h3').innerText = data.spanish.projects[index].title // change title
+      project.querySelector('p').innerText = data.spanish.projects[index].description // change description
+    });
 
-        // buttons
-        buttons.forEach((button) => {
-            const text = button.getAttribute('data-type');
-            const translatedText = data.spanish.buttons[text]
-            
-            let textNode = button.firstChild;
-            textNode.nodeValue = translatedText;
-        });
+    // buttons
+    buttons.forEach((button) => {
+      const text = button.getAttribute('data-type');
+      const translatedText = data.spanish.buttons[text]
 
-        // contact
-        contactMsg.innerText = data.spanish.contact.message;
+      let textNode = button.firstChild;
+      textNode.nodeValue = translatedText;
+    });
 
-    }
-    else{ // switch to english
-        langBtn.innerText = 'eng';
-        
-        // user data
-        userTitle.innerText = data.english.userTitle;
-        userSubtitle.innerText = data.english.userSubtitle;
-        userDescription.innerText = data.english.userDescription;
+    // contact
+    contactMsg.innerText = data.spanish.contact.message;
 
-        // sections
-        sections.forEach((section, index) => {
-            section.innerText = data.english.sections[index];
-        });
+  }
+  else { // switch to english
+    langBtn.innerText = 'eng';
 
-        // projects
-        projects.forEach((project, index) => {
-            project.querySelector('h3').innerText = data.english.projects[index].title // change title
-            project.querySelector('p').innerText = data.english.projects[index].description // change description
-        });
+    // user data
+    userTitle.innerText = data.english.userTitle;
+    userSubtitle.innerText = data.english.userSubtitle;
+    userDescription.innerText = data.english.userDescription;
 
-        // buttons
-        buttons.forEach((button) => {
-            const text = button.getAttribute('data-type');
-            const translatedText = data.english.buttons[text]
-            
-            let textNode = button.firstChild;
-            textNode.nodeValue = translatedText;
-        });
+    // sections
+    sections.forEach((section, index) => {
+      section.innerText = data.english.sections[index];
+    });
 
-        // contact
-        contactMsg.innerText = data.english.contact.message;
-    }
+    // projects
+    projects.forEach((project, index) => {
+      project.querySelector('h3').innerText = data.english.projects[index].title // change title
+      project.querySelector('p').innerText = data.english.projects[index].description // change description
+    });
+
+    // buttons
+    buttons.forEach((button) => {
+      const text = button.getAttribute('data-type');
+      const translatedText = data.english.buttons[text]
+
+      let textNode = button.firstChild;
+      textNode.nodeValue = translatedText;
+    });
+
+    // contact
+    contactMsg.innerText = data.english.contact.message;
+  }
 }
 
 sendBtn.onclick = () => {
-    const message = document.getElementById('message').value;
-    const params = {
-        from_name: 'Contact name',
-        message: message
-    }
-    emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, params)
+  const message = document.getElementById('message').value;
+  const params = {
+    from_name: 'Contact name',
+    message: message
+  }
+  emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, params)
     .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-        alert('Message sent successfully!');
-        document.getElementById('message').value = '';
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Message sent successfully!');
+      document.getElementById('message').value = '';
     }, (error) => {
-        alert('Message could not be sent. Please try again later.');
-        console.log('FAILED...', error);
+      alert('Message could not be sent. Please try again later.');
+      console.log('FAILED...', error);
     });
 }
